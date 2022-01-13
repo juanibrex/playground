d733fbe5fa7678a093e6558b4bb2eeee3eff0872
Juani <onofgo@gmail.com>
2022-01-12 21:55:32 -0300

    First steps in Python

---
+++ CF/python/README.md
@@ -0,0 +1,10 @@
+# Código Facilito
+Listado de cursos y practicas realizadas
+
+## Cursos
+
+### Animaciones CSS
+
+### Fundamentos de Python
++ URL: https://www.youtube.com/watch?v=Nvm7JzhUpc4
++ Fecha: 11/01/2022 
---
+++ CF/python/main.py
@@ -0,0 +1,16 @@
+nombre = input('Ingrese su nombre: ')
+apellido = input('Ingrese su Apellido: ')
+print('Su nombre es', nombre + ' ' + apellido)
+# Este es un comentario de LINEA
+
+'''
+Este es un comentario de 
+BLOQUE
+K asé bb
+'''
+
+'''Este es otro
+comentaario de
+Bloque
+y python se da cuenta y ya
+me tira las otras 4 tildes y el cursor lo pone despues de la 3ra'''
